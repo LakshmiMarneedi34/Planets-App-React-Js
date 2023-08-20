b0cb052de86c06a7f9aaf7ddc36dd3bd32dfde1b
LakshmiMarneedi34 <nagavaralakshmimarneedi5@gmail.com>
2023-08-20 09:58:55 +0000

    PlanetsApp

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,19 @@
+// Write your code here
+import './index.css'
+
+const PlanetItem = props => {
+  const {eachValue} = props
+
+  return (
+    <div className="plant-container">
+      <img
+        src={eachValue.imageUrl}
+        alt={`planet ${eachValue.name}`}
+        className="img"
+      />
+      <h1 className="name-head">{eachValue.name}</h1>
+      <p className="para">{eachValue.description}</p>
+    </div>
+  )
+}
+export default PlanetItem
